08be8d946a7de40af6778fcc4d2979b6f35da254
Zach Ogden <ogdenza@bc.edu>
2019-09-13 00:31:14 -0400

    5.2 Completed

---
+++ DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  To Do List
+//
+//  Created by Zach Ogden on 9/12/19.
+//  Copyright © 2019 Zach Ogden. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: NSObject {
+
+}
---
+++ To Do List/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  To Do List
+//
+//  Created by Zach Ogden on 9/12/19.
+//  Copyright © 2019 Zach Ogden. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var toDoField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+
+
+}
