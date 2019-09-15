b38b5e1b2f55ad625562ae62a64397d6b26edd90
Zach Ogden <ogdenza@bc.edu>
2019-09-15 00:09:45 -0400

    5.4 Completed

--- To Do List/ViewController.swift
+++ To Do List/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
+    
     var toDoArray = ["Learn Swift", "Build Apps", "Change the World!"]
     
     override func viewDidLoad() {
@@ -44,8 +49,33 @@ class ViewController: UIViewController {
                 
             }
         }
+
+    @IBAction func editBarButtonPressed(_ sender: UIBarButtonItem) {
+        if tableView.isEditing{
+            tableView.setEditing(false, animated: true)
+            addBarButton.isEnabled = true
+            editBarButton.title = "Edit"
+        } else {
+            tableView.setEditing(true, animated: true)
+            addBarButton.isEnabled = false
+            editBarButton.title = "Done"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            toDoArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = toDoArray[sourceIndexPath.row]
+        toDoArray.remove(at: sourceIndexPath.row)
+        toDoArray.insert(itemToMove, at: destinationIndexPath.row)
         
     }
+}
     
 
 
